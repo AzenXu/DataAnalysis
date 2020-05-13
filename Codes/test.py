217d924b9fc2b50db82e567d3b4a50a1b4308097
Mier <mier@mier.com>
2020-05-13 16:08:37 +0800

    Tushare测试

--- Codes/test.py
+++ Codes/test.py
@@ -1,1 +1,7 @@
-print('test')
+import tushare as ts
+import os
+
+ts.set_token(os.getenv('TUSHARE_TOKEN'))
+pro = ts.pro_api()
+df = pro.daily(ts_code='002230.sz', start_date='1900-01-01', end_date='2099-01-01')
+print(df)
