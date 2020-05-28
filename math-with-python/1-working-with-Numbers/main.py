5d46b1a127e7a9f18eb6577a3c98c98f3451fe13
Mier <mier@mier.com>
2020-05-28 21:57:45 +0800

    add some interesting functions

--- math-with-python/1-working-with-Numbers/main.py
+++ math-with-python/1-working-with-Numbers/main.py
@@ -11,7 +11,7 @@ def test_fractions():
     f = Fraction(3, 4)
     print(f)
     print(f + 1 + 1.5)
-    print(Fraction(3, 4) + 1 + Fraction(1/4))
+    print(Fraction(3, 4) + 1 + Fraction(1 / 4))
 
 
 def test_complex_number():
@@ -25,7 +25,48 @@ def test_complex_number():
     print(a.real, a.imag)
 
 
+def test_user_input():
+    try:
+        a = input('Input an integer: ')
+        print(type(a))
+        num_a = int(a)
+        print(num_a, type(num_a))
+
+        b = Fraction(input('Input a fraction: '))
+        print(b, type(b))
+
+        z = complex(input('Input a complex number: '))
+        print(z, type(z))
+
+    except ValueError:
+        print('You entered an invalid number')
+
+    except ZeroDivisionError:
+        print('You entered an zero division number')
+
+
+def test_factor_of_integer():
+    def is_factor(a: int, b: int):
+        if b % a == 0:
+            return True
+        else:
+            return False
+
+    def find_factors(from_num: int) -> list:
+        factors = []
+        for num in range(1, from_num + 1):
+            if is_factor(num, from_num):
+                factors.append(num)
+
+        return factors
+
+    # print(is_factor(7, 1024))
+    print(find_factors(9))
+
+
 if __name__ == '__main__':
     # test_exponential()
     # test_fractions()
-    test_complex_number()
+    # test_complex_number()
+    # test_user_input()
+    test_factor_of_integer()
